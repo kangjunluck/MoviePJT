80c973e73e12405a7b9dc9648fff0a61f4ac86df
서강준 <kangjunlucky@gmail.com>
2021-05-25 10:43:00 +0900

    home 제작중

--- final_pjt_front/src/views/Home.vue
+++ final_pjt_front/src/views/Home.vue
@@ -1,26 +1,35 @@
 <template>
-  <div class="home">
+  <div class="home" style="font-family: 'Jua', sans-serif;">
     Home
     <div class="container">
       <div class="row">
         <div class="col-12 col-md-4">
-          <h3>여기는 나닮배</h3>
+          <h2>여기는 나닮배</h2>
           <div class="border-right-danger">
           </div>
         </div>
         <div class="col-12 col-md-4">
-          <h3>오늘의 랜덤 추천 영화</h3>
+          <h2>오늘의 랜덤 추천 영화</h2>
           <div v-if="ranMovie">
-            {{ ranMovie.title }}
+            <div class="card my-3" style="width:18rem ">
+              <img 
+                :src="'https://image.tmdb.org/t/p/w500/' + ranMovie.poster_path" 
+                class="card-img-top" alt="...">
+              <div class="card-body">
+                <h3 class="card-title">{{ ranMovie.title }}</h3>
+                <button class="btn btn-primary" @click="moveMovieDetail(ranMovie)">Detail</button>
+              </div>
+            </div>
           </div>
-          <h3>최고 평점 영화 top5</h3>
-          <div v-for="rankmovie in rankMovies" :key="rankmovie.id">
-            <p>{{ rankmovie.title }}</p>
-            <p>{{ rankmovie.vote_average }}</p>
-          </div>
+          <h2>최고 평점 영화 Top5</h2>
+          <ul class="list">   
+            <li class="item fs-3 d-flex justify-content-start" v-for="rankmovie in rankMovies" :key="rankmovie.id">
+              <span class="movie-item" @click="moveMovieDetail(rankmovie)">{{ rankmovie.title }}  {{ rankmovie.vote_average }}점</span>
+            </li>  
+          </ul>
         </div>
         <div class="col-12 col-md-4 ">
-          <h3>내가 찜한 영화</h3>
+          <h2>내가 찜한 영화</h2>
           <div v-for="likemovie in likeMovies" :key="likemovie.id">
             <h4>{{ likemovie.title }}</h4>
           </div>
@@ -102,6 +111,14 @@ export default {
           console.log(err)
         })
     },
+    moveMovieDetail (movie) {
+      this.$router.push({ 
+        name: "MovieDetail",
+        params: {
+          movie_id: movie.id,
+        }
+      })
+    },
   },
   created () {
     this.getUserId()
@@ -110,3 +127,25 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.list {
+  counter-reset : numbering;
+  list-style-type:none;
+}
+.list .item:before{
+  counter-increment : numbering; 
+  content : counter(numbering);
+  margin-right:10px;
+}
+
+.movie-btn {
+  margin-left: 10px;
+}
+.movie-item {
+  cursor: pointer;
+}
+.movie-item:hover {
+  border: 2px solid dodgerblue;
+}
+</style>
